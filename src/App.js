6f2b5f1b4b7e94a26a4472ef633cee49cec19dbf
Kamil Ronewicz <kamil.ronewicz@x-team.com>
2017-09-16 23:38:55 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Meetjs.pl React Application</h2>
         </div>
         <p className="App-intro">
-          Hello! To get started, edit <code>src/App.js</code> and save to reload.
+          Hello Prime! To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
