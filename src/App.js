bb45aff837dd277881a1b7f696786f6922530543
Kamil Ronewicz <kamil.ronewicz@x-team.com>
2017-09-17 23:42:02 +0200

    Revert changes before presentation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './summit-logo.png';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>React Application</h2>
         </div>
         <p className="App-intro">
-          Hello Gdańsk! How are you?
+          Hello Gdańsk!
         </p>
       </div>
     );
