6c2320d9a488db971512e4b5a8ab0705a0237684
Kamil Ronewicz <kamil.ronewicz@x-team.com>
2017-09-17 15:23:50 +0200

    Add changes at conference

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Meetjs.pl React Application</h2>
+          <h2>Summit.Meetjs.pl React Application</h2>
         </div>
         <p className="App-intro">
           Hello Gdańsk! Good morning all! <code>How are you?</code>.
